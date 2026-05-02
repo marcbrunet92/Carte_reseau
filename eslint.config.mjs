542718adf3bb6e9833ff4104f910bd1552b40c5a
Marc Brunet <marcbrunet92@gmail.com>
2026-05-02 17:54:57 +0200

    update lint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,18 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    rules: {
+      "no-console": "warn",
+      "eqeqeq": "error",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
